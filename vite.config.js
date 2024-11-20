aed7e936d662313e0f8783b270eb0240ab611d9a
MingYuan200 <yuan40133@gmail.com>
2024-11-20 18:10:57 +0800

    1120

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 建立一個開發中的路徑
+  // process.env.NODE_ENV => 取得node環境中的變數
+  // 如果目前的路徑=產品路徑，就執行倉庫的路徑
+  // ? 後面放 '/倉庫名稱/'
+  base:process.env.NODE_ENV === 'production' ? '/my-ToDoList/' :'/',
   plugins: [react()],
 })
